6a651d94405ccd3e3183776cdde7399606127f6e
andrei <angri3@outlook.com>
2018-04-03 18:38:58 +1000

    setup for heroku

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,6 +2,13 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //use build-in js promises
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
+// mongoose.connect('mongodb://localhost:27017/TodoApp');
+// to be used in heroku - 
+// Created acc on mongolab mlab.com
+// created sandbox mongo in it
+// set env variable in heroku MONGOLAB_URI
+// see https://stackoverflow.com/questions/36321385/deploy-nodejs-mongodb-on-heroku-but-need-verify-credit-card-when-install-add-o_
+var URI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/TodoApp';
+console.log(URI); 
+mongoose.connect(URI);
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -12,6 +12,8 @@ var {mongoose} = require('./../db/mongoose');
 
 
 var app = express();
+// to be used by Heroku
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -60,11 +62,15 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
-
+/*
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 })
+*/
 
+app.listen(port, () => {
+	console.log(`Started on port ${port}`);
+})
 
 // For tests
 module.exports = {app};
